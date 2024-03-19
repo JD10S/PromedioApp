22d040279e91dbc044e8c54a8a4000ee707fcb8d
sistemas amedi <dodevp20@outlook.com>
2024-03-19 10:51:34 -0500

    Prueba Cambios

--- Logica/NotasCalculator.cs
+++ Logica/NotasCalculator.cs
@@ -6,6 +6,7 @@ namespace Logica
         public static double CalcularPromedio(int Num1, int Num2, int Num3)
         {
             return (Num1 + Num2 + Num3) / 3.0;
+            //Realizado solid
         }
 
         public static string DeterminarResultado(double promedio)
